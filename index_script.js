4d825005f03571c2cf07352452f12ffdc476fd9c
rachel9786 <145170526+rachel9786@users.noreply.github.com>
2025-01-14 20:00:36 -0500

    Update index_script.js

--- index_script.js
+++ index_script.js
@@ -1,9 +1,3 @@
-
-function displayHomePage() {
-	//document.getElementById("projects").style.display = "block";
-	location.href = "personal_website/home_page.html";
-}
-
 function displayConsoleWebapp() {
-	location.href = "S25_build18_webapp/homepage.html";
+	location.href = "console_webapp_home.html";
 }
